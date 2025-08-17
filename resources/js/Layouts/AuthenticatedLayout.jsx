60cb20650ae51fd76cd0fdfe082fc0ba9e18b365
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-08-17 23:57:51 +0330

    creating channels for brodcast

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,7 +3,7 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function AuthenticatedLayout({ header, children }) {
     const page = usePage();
@@ -12,9 +12,7 @@ export default function AuthenticatedLayout({ header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
 
-    useEffect(() => {
-        console.log("AuthenticatedLayout mounted")
-    },[])
+
 
     return (
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,4 +1,6 @@
 import { usePage } from "@inertiajs/react";
+import Echo from "laravel-echo";
+import { useEffect } from "react";
 
 
 // const { Children } = require("react");
@@ -9,6 +11,22 @@ const ChatLayout = ({children}) => {
     const selectedConversations = page.props.selectedConversations
     console.log("conversations",conversations)
     console.log("selectedConversations",selectedConversations)
+
+    useEffect(() => {
+        window.Echo.join('online')
+            .here((users) => {
+                console.log('Users currently online:', users);
+            })
+            .joining((user) => {
+                console.log('User joined:', user);
+            })
+            .leaving((user) => {
+                console.log('User left:', user);
+            }).error((error) => {
+                console.error('Error joining channel:', error);
+            });
+
+    },[])
     return (
         <>
             Chat Layout
