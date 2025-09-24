f7eef4a0284858a6ff9a15832cd1ca6c3eae5b65
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-09-24 21:04:48 +0330

    group chat channel created

--- routes/channels.php
+++ routes/channels.php
@@ -4,7 +4,7 @@ use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('online', function ($user) {
+Broadcast::channel('online', function (User $user) {
     return $user ? new UserResource($user) : null;
 });
 
@@ -14,3 +14,7 @@ Broadcast::channel(
         return $user->id === $userId1 || $user->id === $userId2 ? $user : null ;
     }
 );
+
+Broadcast::channel('message.group.{groupId}',function(User $user , int $groupId){
+    return $user->groups->contains('id',$groupId) ? $user : null ;
+});
