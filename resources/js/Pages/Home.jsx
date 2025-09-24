2544421d62800a604d73508741d6817576ee8e5d
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-09-24 21:52:32 +0330

    create front for chat message layout

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,12 +1,58 @@
 // import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import ChatLayout from '@/Layouts/ChatLayout';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
 import { Head } from '@inertiajs/react';
+import { useEffect, useRef, useState } from 'react';
 
-function Home(auth) {
+function Home(messages) {
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesCtrRef = useRef(null);
+    useEffect(() => {
+        setLocalMessages(messages);
+    }, [messages])
     return (
         <>
-        Messages
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-slate-200">
+                        Please select conversation to see messages
+                    </div>
+                    <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
+                </div>
+            )}
+            {messages && (
+                <>
+                    <ConversationHeader
+                        selectedConversation={selectedConversation}
+                    />
+
+                    <div
+                        ref={messagesCtrRef}
+                        className="flex-1 overflow-y-auto p-5"
+                    >
+                        {/* {messages} */}
+                        {localMessages.length === 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                <div className="text-lg text-slate-200">
+                                    No messages found
+                                </div>
+                            </div>
+                        )}
+                        {localMessages.length > 0 && (
+                            <div className='flex-1 flex flex-col '>
+                                {localMessages.map((message) => (
+                                    <MessageItem
+                                        key={message.id}
+                                        message={message}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <MessageInput conversation={selectedConversation}/>
+                </>
+            )}
         </>
     );
 }
