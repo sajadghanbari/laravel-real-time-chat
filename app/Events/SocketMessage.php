e95c727e3e1090a7238f5a9670d4deb39d08c4e0
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-09-24 20:54:28 +0330

    pv chat channel created

--- app/Events/SocketMessage.php
+++ app/Events/SocketMessage.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -17,7 +18,7 @@ class SocketMessage
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public Message $message)
     {
         //
     }
--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('online', function ($user) {
     return $user ? new UserResource($user) : null;
 });
+
+Broadcast::channel(
+    'message.user.{userId1}-{userId2}',
+    function (User $user, int $userId1, int $userId2) {
+        return $user->id === $userId1 || $user->id === $userId2 ? $user : null ;
+    }
+);
