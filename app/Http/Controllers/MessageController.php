0b6642679ade4f3656f6d0dead9f586027b92dc9
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-09-27 21:10:06 +0330

    emoji created and adding upload files

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -37,7 +37,7 @@ class MessageController extends Controller
     {
         $messages = Message::where('group_id', $group->id)
             ->latest()
-            ->paginate(20);
+            ->paginate(100);
 
         return inertia('Home', [
             'selectedConversation' => $group->toConversationArray(),
@@ -48,20 +48,20 @@ class MessageController extends Controller
     public function loadOlder(Message $message)
     {
         if ($message->group_id) {
-            $messages = Message::where('group_id', $message->group_id)
-                ->where('created_at', '<', $message->created_at)
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where('group_id', $message->group_id)
                 ->latest()
-                ->paginate(50);
+                ->paginate(100);
         } else {
-            $messages = Message::where(function ($query) use ($message) {
-                $query->where('sender_id', $message->sender_id)
-                    ->where('receiver_id', $message->receiver_id);
-            })->orWhere(function ($query) use ($message) {
-                $query->where('sender_id', $message->receiver_id)
-                    ->where('receiver_id', $message->sender_id);
-            })->where('created_at', '<', $message->created_at)
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where(function ($query) use ($message) {
+                    $query->where('sender_id', $message->sender_id)
+                        ->where('receiver_id', $message->receiver_id)
+                        ->orWhere('sender_id', $message->receiver_id)
+                        ->where('receiver_id',$message->sender_id);
+            })
             ->latest()
-            ->paginate(10);
+            ->paginate(100);
         }
         
         return MessageResource::collection($messages);
--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 import {
     PaperClipIcon,
     PhotoIcon,
@@ -8,11 +9,26 @@ import {
 } from "@heroicons/react/24/solid";
 import NewMessageInput from "./NewMessageInput";
 import axios from "axios";
+import EmojiPicker from "emoji-picker-react";
+
 
 const MessageInput = ({ conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
+    const [chosenFiles, setChosenFiles] = useState(0);
+    const [uploadProgress, setUploadProgress] = useState(0);
     const [inputErrorMessage, setInputErrorMessage] = useState("");
     const [messageSending, setMessageSending] = useState(false);
+    const onFileChange = (ev) => {
+        const files = ev.target.files;
+
+        const updatedFiles = [...files].map((file) => {
+            return {
+                file: file,
+                url: URL.createObjectURL(file),
+            }
+        });
+        setChosenFiles(updatedFiles);
+    }
     const onSendClick = () => {
         if (newMessage.trim() === "") {
             setInputErrorMessage("Message cannot be empty");
@@ -23,15 +39,15 @@ const MessageInput = ({ conversation = null }) => {
             return;
         }
         const formData = new FormData();
-        formData.append("message" , newMessage)
-        if (conversation.is_user){
-            formData.append("receiver_id" , conversation.id)
-        }else if (conversation.is_group){
-            formData.append("group_id",conversation);
+        formData.append("message", newMessage)
+        if (conversation.is_user) {
+            formData.append("receiver_id", conversation.id)
+        } else if (conversation.is_group) {
+            formData.append("group_id", conversation);
         }
 
         setMessageSending(true);
-        axios.post(route("message.store"),formData,{
+        axios.post(route("message.store"), formData, {
             onUploadProgress: (progressEvent) => {
                 console.log("Upload Progress: " + Math.round((progressEvent.loaded / progressEvent.total) * 100) + "%");
             }
@@ -42,59 +58,92 @@ const MessageInput = ({ conversation = null }) => {
             setMessageSending(false);
         });
     }
-        return (
-            <div className="flex flex-wrap items-start border-t border-slate-700 py-3 ">
-                <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
-                    <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-                        <PaperClipIcon className="w-6" />
-                        <input
-                            type="file"
-                            multiple
-                            className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
-                        />
-                    </button>
-                    <button className="p-1 text-gray-400 hover:text-gray-300 relative">
-                        <PhotoIcon className="w-6" />
-                        <input
-                            type="file"
-                            multiple
-                            accept="image/*"
-                            className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
-                        />
-                    </button>
-                </div>
-                <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
-                    <div className="flex">
-                        <NewMessageInput
-                            value={newMessage}
-                            onSend = {onSendClick}
-                            onChange={(ev) => setNewMessage(ev.target.value)}
-                        />
-                        <button onClick={onSendClick} className="btn btn-info rounded-1-none">
-                            {messageSending && (
-                                <span className="loading loading-spinner loading-xs"></span>
-                            )}
-                            <PaperAirplaneIcon className="w-4" />
-                            <span className="hidden sm:inline">Send</span>
-                        </button>
-                    </div>
-                    {inputErrorMessage && (
-                        <p className="text-red-500 text-xs">
-                            {inputErrorMessage}
-                        </p>
-                    )}
-                </div>
-                <div className="order-3 flex xs:order-3 p-2">
-                    <button className="p-1 text-gray-400 hover:text-gray-300 ">
-                        <FaceSmileIcon className="w-6 h-6" />
-                    </button>
-                    <button className="p-1 text-gray-400 hover:text-gray-300 ">
-                        <HandThumbUpIcon className="w-6 h-6" />
-                    </button>
-                </div>
-
-            </div>
-        );
+    const onLikeClick = () => {
+        if (messageSending) {
+            return;
+        }
+        const data = {
+            message: "👍"
+        }
+        if (conversation.is_user) {
+            data["receiver_id"] = conversation.id
+        } else if (conversation.is_group) {
+            data["group_id"] = conversation;
+        }
+        axios.post(route("message.store"), data)
     }
 
-    export default MessageInput;
+    const sendRequest = (formData) => {
+        axios.post(route("message.store"), formData, {
+            onUploadProgress: (progressEvent) => {
+                console.log("Upload Progress: " + Math.round((progressEvent.loaded / progressEvent.total) * 100) + "%");
+            }
+        }).then((response) => {
+            setNewMessage("");
+            setMessageSending(false);
+        }).catch((error) => {
+            setMessageSending(false);
+        });
+    }
+    return (
+        <div className="flex flex-wrap items-start border-t border-slate-700 py-3 ">
+            <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
+                <button className="p-1 text-gray-400 hover:text-gray-300 relative">
+                    <PaperClipIcon className="w-6" />
+                    <input
+                        type="file"
+                        multiple
+                        className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
+                    />
+                </button>
+                <button className="p-1 text-gray-400 hover:text-gray-300 relative">
+                    <PhotoIcon className="w-6" />
+                    <input
+                        type="file"
+                        multiple
+                        accept="image/*"
+                        className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
+                    />
+                </button>
+            </div>
+            <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
+                <div className="flex">
+                    <NewMessageInput
+                        value={newMessage}
+                        onSend={onSendClick}
+                        onChange={(ev) => setNewMessage(ev.target.value)}
+                    />
+                    <button onClick={onSendClick} className="btn btn-info rounded-1-none">
+                        {messageSending && (
+                            <span className="loading loading-spinner loading-xs"></span>
+                        )}
+                        <PaperAirplaneIcon className="w-4" />
+                        <span className="hidden sm:inline">Send</span>
+                    </button>
+                </div>
+                {inputErrorMessage && (
+                    <p className="text-red-500 text-xs">
+                        {inputErrorMessage}
+                    </p>
+                )}
+            </div>
+
+            <div className="order-3 flex xs:order-3 p-2">
+                <Popover className="relative">
+                    <Popover.Button className="p-1 text-gray-400 hover:text-gray-300 ">
+                        <FaceSmileIcon className="w-6 h-6" />
+                    </Popover.Button>
+                    <Popover.Panel className="absolute z-10 right-0 bottom-full">
+                        <EmojiPicker theme="dark" onEmojiClick={ev => setNewMessage(newMessage + ev.emoji)} />
+                    </Popover.Panel>
+                </Popover>
+                <button onClick={onLikeClick} className="p-1 text-gray-400 hover:text-gray-300 ">
+                    <HandThumbUpIcon className="w-6 h-6" />
+                </button>
+            </div>
+
+        </div>
+    );
+}
+
+export default MessageInput;
